7163f09892acfe567a5b9ff5e4b80b756a2e180f
malithsathmina <malithsathmina8@gmail.com>
2025-03-22 10:43:54 +0530

    build the class

--- CopyArmy.java
+++ CopyArmy.java
@@ -1,3 +1,18 @@
+package OOpProgect;
+
+//Class for take a copy of a army elements fighting attributes.
 public class CopyArmy {
-    
+  private double Attack ;
+  private double Defence ;
+  private double Health;
+  private double Speed ;
+
+  //take a copy
+  public CopyArmy(Army army) {
+      Attack = army.getAttack();
+      Defence = army.getDefence();
+      Health = army.getHealth();
+      Speed = army.getSpeed();
+  }
+  
 }
