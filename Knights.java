1f6d244abbd470eee2199f9fd6d2cf0a050ddf96
malithsathmina <malithsathmina8@gmail.com>
2025-03-25 08:40:33 +0530

    add more methods

--- Knights.java
+++ Knights.java
@@ -42,4 +42,94 @@ public class Knights implements Army {
         Knights Sq = new Knights("Squire", 85, 8.0, 9.0, 7.0, 8.0, "Marshlanders");
         return Sq;
     }
+    public static Knights Cavalier() {
+        // Creating a Cavalier knight
+        Knights Ca = new Knights("Cavalier", 110, 10.0, 12.0, 7.0, 10.0, "Highlanders");
+        return Ca;
+    }
+
+    public static Knights Templar() {
+        // Creating a Templar knight
+        Knights Te = new Knights("Templar", 155, 14.0, 16.0, 12.0, 12.0, "Sunchildren");
+        return Te;
+    }
+
+    public static Knights Zoro() {
+        // Creating a Zoro knight
+        Knights Zr = new Knights("Zoro", 180, 17.0, 16.0, 13.0, 14.0, "Highlanders");
+        return Zr;
+    }
+
+    public static Knights Swiftblade() {
+        // Creating a Swiftblade knight
+        Knights Sw = new Knights("Swiftblade", 250, 18.0, 20.0, 17.0, 13.0, "Marshlanders");
+        return Sw;
+    }
+
+    // Getters to access attributes of knights.
+    public String getCategory() {
+        return Category;
+    }
+
+    public int getPrice() {
+        return Price;
+    }
+
+    public double getAttack() {
+        return Attack;
+    }
+
+    public double getDefence() {
+        return Defence;
+    }
+
+    public double getHealth() {
+        return Health;
+    }
+
+    public double getSpeed() {
+        return Speed;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public boolean getArmer() {
+        return armer;
+    }
+
+    public boolean getArtefacts() {
+        return artefacts;
+    }
+
+    // Setters to set the attributes of knights.
+    public void setPrice(int price) {
+        this.Price = price;
+    }
+
+    public void setAttack(double attack) {
+        this.Attack = attack;
+    }
+
+    public void setDefence(double defence) {
+        this.Defence = defence;
+    }
+
+    public void setHealth(double health) {
+        this.Health = health;
+    }
+
+    public void setSpeed(double speed) {
+        this.Speed = speed;
+    }
+
+    public void setArmer() {
+        armer = true;
+    }
+
+    public void setArtefacts() {
+        artefacts = true;
+    }
+
 }
