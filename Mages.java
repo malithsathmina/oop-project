a70234b4ced615d2fabd41c0717894b7d12b2c05
malithsathmina <malithsathmina8@gmail.com>
2025-03-26 13:55:41 +0530

    add some methode

--- Mages.java
+++ Mages.java
@@ -25,5 +25,39 @@ public class Mages implements Army {
         this.Category = Category;
     }
 
-    
+    // Method to print details of different types of mages.
+    public static void print() {
+        // Printing details of different mage types
+        Army.printDetails(Warlock());
+        Army.printDetails(Illusionist());
+        Army.printDetails(Enchanter());
+        Army.printDetails(Conjurer());
+        Army.printDetails(Eldritch());
+    }
+
+    // Creating different types of mages.
+    public static Mages Warlock() {
+        // Creating a Warlock mage
+        Mages Wl = new Mages("Warlock", 100, 12.0, 7.0, 10.0, 12.0, "Marshlanders");
+        return Wl;
+    }
+
+    public static Mages Illusionist() {
+        // Creating an Illusionist mage
+        Mages Il = new Mages("Illusionist", 120, 13.0, 8.0, 12.0, 14.0, "Mystics");
+        return Il;
+    }
+
+    public static Mages Enchanter() {
+        // Creating an Enchanter mage
+        Mages Enc = new Mages("Enchanter", 160, 16.0, 10.0, 13.0, 16.0, "Highlanders");
+        return Enc;
+    }
+
+    public static Mages Conjurer() {
+        // Creating a Conjurer mage
+        Mages Cnj = new Mages("Conjurer", 195, 18.0, 15.0, 14.0, 12.0, "Highlanders");
+        return Cnj;
+    }
+
 }
